a9c6287f1483b4649726a8b3d40259bb2af5805d
Noah Schill <noahrschill@gmail.com>
2024-12-28 12:06:08 -0800

    remove client

--- trip_check_api_client/__init__.py
+++
@@ -1,8 +0,0 @@
-
-""" A client library for accessing TripCheck API v1 """
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- trip_check_api_client/models/incidents_incident_location_direction.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class IncidentsIncidentLocationDirection(str, Enum):
-    BOTH_DIRECTIONS = "BOTH_DIRECTIONS"
-    ONE_DIRECTION = "ONE_DIRECTION"
-
-    def __str__(self) -> str:
-        return str(self.value)
