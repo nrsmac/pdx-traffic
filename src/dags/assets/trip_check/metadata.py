59a0af7ddffd535efee84853984fc23c568d250f
Noah Schill <noahrschill@gmail.com>
2025-01-24 15:49:13 -0800

    clean up groups

--- src/dags/assets/trip_check/metadata.py
+++ src/dags/assets/trip_check/metadata.py
@@ -7,25 +7,31 @@ import trip_check_api.metadata as md
 def incident_event_types() -> pd.DataFrame:
     return md.get_incident_event_types()
 
+
 @asset
 def incident_event_subtypes() -> pd.DataFrame:
     return md.get_incident_event_subtypes()
+
 
 @asset
 def road_condition_descriptions() -> pd.DataFrame:
     return md.get_road_condition_descriptions()
 
+
 @asset
 def weather_condition_descriptions() -> pd.DataFrame:
     return md.get_weather_condition_descriptions()
+
 
 @asset
 def routes() -> pd.DataFrame:
     return md.get_routes()
 
+
 @asset
 def travel_impact_descriptions() -> pd.DataFrame:
     return md.get_travel_impact_descriptions()
+
 
 @asset
 def travel_incident_types() -> pd.DataFrame:
